035edaad79f3eb10b15001e841c5fc4d2886058b
Juliano Carneiro <julianoomc@gmail.com>
2023-03-02 00:24:46 -0300

    imports para detecção facial e evento de liberação do botão

--- classes/class-01/pages/video-player/src/controller.js
+++ classes/class-01/pages/video-player/src/controller.js
@@ -1,9 +1,11 @@
 export default class Controller {
     #view
     #service
-    constructor({ view, service }) {
+    #worker
+    constructor({ view, service, worker }) {
         this.#view = view
         this.#service = service
+        this.#worker = this.#configureWorker(worker)
 
         this.#view.configureOnBtnClick(this.onBtnStart.bind(this))
     }
@@ -12,6 +14,17 @@ export default class Controller {
         const controller = new Controller(deps)
         controller.log('not yet detecting eye blink! click in the button to start')
         return controller.init()
+    }
+
+    #configureWorker(worker) {
+        worker.onmessage = (msg) => {
+            if('READY' === msg.data)  {
+                this.#view.enableButton()
+                return
+            }
+        }
+
+        return worker
     }
 
     async init() {
--- classes/class-01/pages/video-player/src/factory.js
+++ classes/class-01/pages/video-player/src/factory.js
@@ -21,7 +21,6 @@ async function getWorker() {
 }
 
 const worker = await getWorker()
-worker.postMessage('hey from factory!')
 
 const camera = await Camera.init()
 const [rootPath] = window.location.href.split('/pages/')
@@ -29,7 +28,8 @@ const factory = {
   async initialize() {
     return Controller.initialize({
       view: new View(),
-      service: new Service({})
+      service: new Service({}),
+      worker
     })
   }
 }
