f9eb13b84edffe9e5739f6fb01e80e846d284713
Juliano Carneiro <julianoomc@gmail.com>
2023-03-01 23:46:29 -0300

    cria alternativa para uso da câmera no firefox

--- classes/class-01/pages/video-player/src/factory.js
+++ classes/class-01/pages/video-player/src/factory.js
@@ -1,7 +1,27 @@
 import Camera from "../../../lib/shared/camera.js"
+import { supportsWorkerType } from "../../../lib/shared/util.js"
 import Controller from "./controller.js"
 import Service from "./service.js"
 import View from "./view.js"
+
+async function getWorker() {
+    if (supportsWorkerType) {
+        console.log('suporta!')
+        const worker = new Worker('./src/worker.js', { type: 'module' })
+        return worker
+    }
+    
+    const workerMock = {
+        async postMessage() {},
+        onmessage(msg) {}
+    }
+
+    console.log('não suporta!')
+    return workerMock
+}
+
+const worker = await getWorker()
+worker.postMessage('hey from factory!')
 
 const camera = await Camera.init()
 const [rootPath] = window.location.href.split('/pages/')
