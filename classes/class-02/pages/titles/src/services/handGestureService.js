287bd07fab6edceb954bcb4e695128f9535204cc
Juliano Carneiro <julianoomc@gmail.com>
2023-03-02 20:59:10 -0300

    captura movimentos de mão aberta e fechada através dos keypoints

--- classes/class-02/pages/titles/src/services/handGestureService.js
+++ classes/class-02/pages/titles/src/services/handGestureService.js
@@ -1,12 +1,43 @@
+import { knownGestures, gestureStrings } from "../util/gestures.js"
+
 export default class HandGestureService {
     #fingerpose
+    #gestureEstimator
     #handPoseDetection
     #handsVersion
     #detector = null
     constructor({ fingerpose, handPoseDetection, handsVersion }) {
-        this.#fingerpose = fingerpose
+        this.#gestureEstimator = new fingerpose.GestureEstimator(knownGestures)
         this.#handPoseDetection = handPoseDetection
         this.#handsVersion = handsVersion
+    }
+
+    async estimate(keypoints3D) {
+        const predictions = await this.#gestureEstimator.estimate(
+            this.#getLandmarksFromKeypoints(keypoints3D),
+            9 // porcentagem de confiança do gesto - 90%
+        )
+        return predictions.gestures
+    }
+
+    async * detectGestures(predictions) {
+        for (const hand of predictions) {
+            if (!hand.keypoints3D) continue
+
+            const gestures = await this.estimate(hand.keypoints3D)
+            if (!gestures.length) continue
+
+            const result = gestures.reduce(
+                (previous, current) => (previous.score > current.score) ? previous : current
+            )
+            
+            console.log('detected', gestureStrings[result.name])
+        }
+    }
+
+    #getLandmarksFromKeypoints(keypoints3D) {
+        return keypoints3D.map(keypoint =>
+            [keypoint.x, keypoint.y, keypoint.z])
     }
 
     async estimateHands(video) {
@@ -17,7 +48,7 @@ export default class HandGestureService {
 
     async initializeDetector() {
         if (this.#detector) return this.#detector
-       
+
         const detectorConfig = {
             runtime: 'mediapipe', // or 'tfjs',
             solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${this.#handsVersion}`,
