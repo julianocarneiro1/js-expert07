824cbfdfd5b96c8664c891777d872043679b3a51
Juliano Carneiro <julianoomc@gmail.com>
2023-03-02 20:15:00 -0300

    configura imports e cria detector para identificação das mãos via câmera

--- classes/class-02/pages/titles/src/controllers/handGestureController.js
+++ classes/class-02/pages/titles/src/controllers/handGestureController.js
@@ -7,6 +7,7 @@ export default class HandGestureController {
     }
 
     async init() {
+        await this.#service.initializeDetector()
     }
 
     static async initialize(deps) {
--- classes/class-02/pages/titles/src/services/handGestureService.js
+++ classes/class-02/pages/titles/src/services/handGestureService.js
@@ -1,1 +1,29 @@
-export default class HandGestureService {}
+export default class HandGestureService {
+    #fingerpose
+    #handPoseDetection
+    #handsVersion
+    #detector = null
+    constructor({ fingerpose, handPoseDetection, handsVersion }) {
+        this.#fingerpose = fingerpose
+        this.#handPoseDetection = handPoseDetection
+        this.#handsVersion = handsVersion
+    }
+
+    async initializeDetector() {
+        if (this.#detector) return this.#detector
+       
+        const detectorConfig = {
+            runtime: 'mediapipe', // or 'tfjs',
+            solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${this.#handsVersion}`,
+            // full é o mais pesado e mais preciso, lite é mais leve
+            modelType: 'lite',
+            maxHands: 2,
+        }
+        this.#detector = await this.#handPoseDetection.createDetector(
+            this.#handPoseDetection.SupportedModels.MediaPipeHands,
+            detectorConfig
+        )
+
+        return this.#detector
+    }
+}
