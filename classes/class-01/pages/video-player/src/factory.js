d898e846cb4f029aec5d6c713724223023217a0e
Juliano Carneiro <julianoomc@gmail.com>
2023-03-02 00:02:09 -0300

    cria botão e função de log para status de reconhecimento de olhos

--- classes/class-01/pages/video-player/src/factory.js
+++ classes/class-01/pages/video-player/src/factory.js
@@ -28,7 +28,7 @@ const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initialize() {
     return Controller.initialize({
-      view: new View({}),
+      view: new View(),
       service: new Service({})
     })
   }
