da365c9848418489426c13054e6731450cbe136d
Juliano Carneiro <julianoomc@gmail.com>
2023-03-02 11:07:01 -0300

    adapta worker mock para funcionar com firefox

--- classes/class-01/pages/video-player/src/factory.js
+++ classes/class-01/pages/video-player/src/factory.js
@@ -5,19 +5,41 @@ import Service from "./service.js"
 import View from "./view.js"
 
 async function getWorker() {
-    if (supportsWorkerType) {
-        console.log('suporta!')
-        const worker = new Worker('./src/worker.js', { type: 'module' })
-        return worker
-    }
-    
-    const workerMock = {
-        async postMessage() {},
-        onmessage(msg) {}
-    }
+  if (supportsWorkerType) {
+    console.log('initializing esm workers!')
+    const worker = new Worker('./src/worker.js', { type: 'module' })
+    return worker
+  }
 
-    console.log('não suporta!')
-    return workerMock
+  console.warn(`Your browser doesn't support esm modules on webworkers!`)
+  console.warn(`Importing libraries...`)
+  await import("https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js")
+  await import("https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js")
+
+  console.warn(`Using worker mock instead!`)
+  const service = new Service({
+    faceLandmarksDetection: window.faceLandmarksDetection
+  })
+
+  const workerMock = {
+    async postMessage(video) {
+      const blinked = await service.handBlinked(video)
+      if(!blinked) return
+      workerMock.onmessage({ data: { blinked } })
+    },
+    // vai ser sobrescrito pela controller
+    onmessage(msg) { }
+  }
+
+  console.log('loading tf model...')
+  await service.loadModel()
+  console.log('tf model loaded!')
+  
+  setTimeout(() => worker.onmessage({ data: 'READY' }), 500)
+
+  return workerMock
 }
 
 const worker = await getWorker()
